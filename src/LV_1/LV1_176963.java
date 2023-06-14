efb1e428459ddd5a5a8dea0b486fc344e28b632f
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-06-14 20:49:32 +0900

    first commit

---
+++ src/LV_1/LV1_176963.java
@@ -0,0 +1,33 @@
+package LV_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LV1_176963 {
+    public int[] lv_176963(String[] args) {
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {
+                {"may", "kein", "kain", "radi"},
+                {"may", "kein", "brin", "deny"},
+                {"kon", "kain", "may", "coni"}
+        };
+        int[] answer = new int[name.length];
+
+        Map<String, Integer> a = new HashMap<String, Integer>();
+        for (int i = 0; i < name.length; i++) {
+            a.put(name[i], yearning[i]);
+        }
+//        for (String key : a.keySet()) {
+//            System.out.println("Key: " + key + ", Value: " + a.get(key));
+//        }
+        for (int i = 0; i < photo.length; i++) {
+            for (int j = 0; j < photo[i].length; j++) {
+                if (a.containsKey(photo[i][j])) {
+                    answer[i] += a.get(photo[i][j]);
+                }
+            }
+        }
+        return answer;
+    }
+}
---
+++ src/LV_1/Main.java
@@ -0,0 +1,11 @@
+package LV_1;
+
+
+import java.util.*;
+
+// Map을 사용하여 풀어볼 것
+public class Main {
+
+}
+
+
